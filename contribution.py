e4fe42f1a50fcba3771b8d1a34c10b978fbe4881
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-05 19:29:24 +0900

    fetch by dates

--- contribution.py
+++ contribution.py
@@ -1,8 +1,16 @@
 import datetime
-from dataclasses import dataclass
 
 
-@dataclass
 class Contribution:
     date: datetime.date
     commits: int
+
+    def __init__(self, date, commits):
+        self.date = date
+        self.commits = commits
+
+    def __hash__(self):
+        return hash(self.date)
+
+    def __eq__(self, other):
+        return self.date == other.date
--- github_charts.py
+++ github_charts.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 
 import requests
 
@@ -6,13 +6,20 @@ from contribution import Contribution
 from html_parser import ContributionHTMLParser
 
 
-def fetch() -> List[Contribution]:
-    response = requests.get('https://github.com/users/CORDEA/contributions')
+def __fetch(date: str) -> List[Contribution]:
+    response = requests.get('https://github.com/users/CORDEA/contributions?to=' + date)
     body = response.text
     parser = ContributionHTMLParser()
     parser.feed(body)
     return parser.contributions
 
 
+def __fetch_all() -> Set[Contribution]:
+    conts = []
+    for i in range(5, 10):
+        conts.extend(__fetch('201' + str(i) + '-01-01'))
+    return set(conts)
+
+
 if __name__ == '__main__':
-    print(fetch())
+    __fetch_all()
