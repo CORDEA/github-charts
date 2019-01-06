f1f5c7e3dd17e27673dae3da35dbf756bf9f4156
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-06 15:41:56 +0900

    draw stripplot

--- plotter.py
+++ plotter.py
@@ -1,10 +1,12 @@
 import datetime
-from typing import Dict
+from typing import Dict, Set
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+from contribution import Contribution
 
 
 def draw_line(data: Dict[datetime.date, int]):
@@ -27,3 +29,16 @@ def draw_heatmap(data: Dict[datetime.date, int]):
     f, ax = plt.subplots()
     sns.heatmap(frame, annot=True, fmt='d', linewidths=.5, ax=ax)
     plt.show()
+
+
+def draw_stripplot(data: Set[Contribution]):
+    sns.set()
+    items = []
+    for c in data:
+        items.append((c.date.year, c.date.month, c.date.day, c.commits))
+    data = pd.DataFrame(items, columns=['year', 'month', 'day', 'commits'])
+    _, ax = plt.subplots()
+    sns.stripplot(x='year', y='commits', data=data, jitter=True, alpha=.15, zorder=1, ax=ax)
+    plot = sns.pointplot(
+        x='year', y='commits', data=data, palette='dark', markers='d', scale=.75, join=False, ci=None, ax=ax)
+    plot.get_figure().savefig('stripplot.png')
